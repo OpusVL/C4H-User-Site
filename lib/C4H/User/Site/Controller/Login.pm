67b9fa713d806b8e4b3cad49433cb7aa0da16e0c
Alastair McGowan-Douglas <alastair.mcgowan@opusvl.com>
2015-06-19 12:45:27 +0100

    Implement password reset into login controller

--- lib/C4H/User/Site/Controller/Login.pm
+++ lib/C4H/User/Site/Controller/Login.pm
@@ -2,7 +2,7 @@ package C4H::User::Site::Controller::Login;
 
 use Moose;
 use C4H::User::Site::HTML::FormHandler::Bootstrap3;
-BEGIN {extends 'CatalystX::SimpleLogin::Controller::Login';}
+BEGIN {extends 'OpusVL::AppKitX::PasswordReset::Controller::PasswordReset';}
 
 has '+render_login_form_stash_key' => (
     default => 'render_form'
@@ -12,10 +12,35 @@ has '+login_form_class' => (
     default => 'C4H::User::Site::HTML::FormHandler::Bootstrap3'
 );
 
+has user_email_field => (
+    is => 'ro',
+    isa => 'Str',
+    default => 'username',
+);
+
+has user_name_field => (
+    is => 'ro',
+    isa => 'Str',
+    default => 'full_name',
+);
+
+
 after 'login' => sub {
     my ($self, $c) = @_;
     $c->detach(qw/Controller::Root default/);
 };
+
+after 'reset_password' => sub {
+    my ($self, $c) = @_;
+    $c->detach(qw/Controller::Root default/);
+};
+
+after 'reset' => sub {
+    my ($self, $c) = @_;
+
+    $c->detach(qw/Controller::Root default/);
+};
+
 
 1;
 
