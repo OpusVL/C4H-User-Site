fbd4c0ce767a217d7205f3fead3abfae78b5c8e3
Colin Newell <colin.newell@gmail.com>
2015-11-24 19:53:41 +0000

    Bumped version number

--- lib/C4H/User/Site.pm
+++ lib/C4H/User/Site.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 use C4H::User::Site::Builder;
 
-our $VERSION = "0.16";
+our $VERSION = "0.17";
 
 my $builder = C4H::User::Site::Builder->new(
     appname => __PACKAGE__,
